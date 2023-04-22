8bf6df486fa506b89a6931f31ffb035e051ab1d8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-04-22 08:50:01 +0100

    add tsdoc

--- packages/loaders/src/Wrapper.tsx
+++ packages/loaders/src/Wrapper.tsx
@@ -1,3 +1,30 @@
+/**
+ * Wrapper component for loaders, preserves AnimatePresence when unmounting the loader to prevent the loader from disappearing abruptly.
+ * @module @bui/loaders
+ * @category Components
+ * @subcategory Loaders
+ * @param {boolean} isLoading - Whether the loader is loading or not.
+ * @param {ReactNode} loader - The loader component.
+ * @returns {JSX.Element} - The loader component wrapped in AnimatePresence.
+ *
+ * @example
+ * import LoaderWrapper, { FIFA23 } from "@bui/loaders";
+ *
+ * const App = () => {
+ *  const [isLoading, setIsLoading] = useState(false);
+ *
+ *  return (
+ *    <div>
+ *        <button onClick={() => setIsLoading(!isLoading)}>Toggle</button>
+ *        <LoaderWrapper isLoading={isLoading} loader={<FIFA23 />} />
+ *    </div>
+ *        );
+ * };
+ *
+ * export default App;
+ *
+ */
+
 import type { ReactNode } from "react";
 
 import { AnimatePresence } from "framer-motion";
