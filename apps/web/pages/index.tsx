245265ab6f503d42883bf89503b0aa0f894b1708
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-04-28 08:31:02 +0100

    disable loader

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import LoaderWrapper, { ComingUpNext } from "@bui/loaders";
+// import LoaderWrapper, { ComingUpNext } from "@bui/loaders";
 import { MD3 } from "@bui/buttons";
 
 export default function Web() {
@@ -9,7 +9,7 @@ export default function Web() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#002]">
       <MD3 onClick={() => setIsLoading(!isLoading)}>Submit</MD3>
-      <LoaderWrapper isLoading={isLoading} loader={<ComingUpNext />} />
+      {/* <LoaderWrapper isLoading={isLoading} loader={<ComingUpNext />} /> */}
     </div>
   );
 }
