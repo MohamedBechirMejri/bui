f6c6039c6121953fec95b8ff51fc20003f72484d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-04-28 07:47:05 +0100

    style test page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -7,16 +7,7 @@ export default function Web() {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        backgroundColor: "#002",
-      }}
-    >
+    <div className="flex flex-col items-center justify-center h-screen bg-[#002]">
       <MD3 onClick={() => setIsLoading(!isLoading)}>Submit</MD3>
       <LoaderWrapper isLoading={isLoading} loader={<ComingUpNext />} />
     </div>
