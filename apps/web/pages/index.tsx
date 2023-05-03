e34a6014afc4d34c695a41b4cc228cbb4b15fc82
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-03 02:37:26 +0100

    Add Ripple button

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 // import LoaderWrapper, { ComingUpNext } from "@bui/loaders";
-import { Magic } from "@bui/buttons";
+import { Ripple } from "@bui/buttons";
 
 export default function Web() {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#111]">
-      <Magic onClick={() => setIsLoading(!isLoading)}>Generate Site</Magic>
+      <Ripple onClick={() => setIsLoading(!isLoading)}>Generate Site</Ripple>
       {/* <LoaderWrapper isLoading={isLoading} loader={<ComingUpNext />} /> */}
     </div>
   );
---
+++ packages/buttons/src/Ripple.tsx
@@ -0,0 +1,80 @@
+import { Suspense, useEffect, useRef, useState } from "react";
+import {
+  useSpring,
+  useTransform,
+  motion,
+  MotionConfig,
+  useMotionValue,
+  AnimatePresence,
+} from "framer-motion";
+import useMeasure from "react-use-measure";
+import { motion as motion3d } from "framer-motion-3d";
+import { Canvas } from "@react-three/fiber";
+
+export const Ripple = ({
+  children,
+  color = "#2882fc",
+  ...props
+}: {
+  color?: string;
+  children: React.ReactNode;
+}) => {
+  const transition = { type: "spring", duration: 0.7, bounce: 0.2 };
+
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => setIsHovering(true);
+  const handleMouseLeave = () => setIsHovering(false);
+
+  useEffect(() => {
+    if (ref.current) {
+      // @ts-ignore
+      const { width, height } = ref.current.getBoundingClientRect();
+      setWidth(width);
+      setHeight(height);
+    }
+  }, []);
+
+  return (
+    <MotionConfig transition={transition}>
+      <motion.button
+        ref={ref}
+        style={{
+          appearance: "none",
+          outline: "none",
+          color: "#fff",
+          padding: "1em 2em",
+          fontFamily: "'Open Sans', Roboto, sans-serif",
+          fontSize: "1rem",
+          fontWeight: "bold",
+          cursor: "pointer",
+          position: "relative",
+          userSelect: "none",
+          borderRadius: "400px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "between",
+          gap: "0.75em",
+          WebkitTapHighlightColor: "transparent",
+          border: `3px solid ${color}`,
+        }}
+        initial={{ backgroundColor: color + "22" }}
+        whileHover={{ backgroundColor: color + "44" }}
+        layout
+        whileTap={{ backgroundColor: color + "66" }}
+        transition={{ duration: 0.2 }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        {...props}
+      >
+        <motion.p>{children}</motion.p>
+        <AnimatePresence></AnimatePresence>
+      </motion.button>
+    </MotionConfig>
+  );
+};
