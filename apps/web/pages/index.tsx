4bf3762711c46feb84dde01fc7d002210a723092
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-05 17:52:52 +0100

    remove electric button

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 // import LoaderWrapper, { ComingUpNext } from "@bui/loaders";
-import { Ripple, MD3, Electric } from "@bui/buttons";
+import { Ripple, MD3 } from "@bui/buttons";
 
 export default function Web() {
   const [isLoading, setIsLoading] = useState(false);
@@ -10,7 +10,6 @@ export default function Web() {
     <div className="flex flex-col items-center justify-center h-screen bg-[#111]">
       {/* <MD3 onClick={() => setIsLoading(!isLoading)}>Generate Site</MD3> */}
       {/* <Ripple onClick={() => setIsLoading(!isLoading)}>Submit</Ripple> */}
-      <Electric onClick={() => setIsLoading(!isLoading)}>Submit</Electric>
       {/* <LoaderWrapper isLoading={isLoading} loader={<ComingUpNext />} /> */}
     </div>
   );
--- packages/buttons/src/index.tsx
+++ packages/buttons/src/index.tsx
@@ -3,4 +3,3 @@ export * from "./Glow";
 export * from "./MD3";
 export * from "./Magic";
 export * from "./Ripple";
-export * from "./Electric";
