ff5df63d00a68d8dcb62287d3820099ebe0b6ba5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-06 18:59:57 +0100

    test buttons - they all need fixing

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 
 // import LoaderWrapper, { ComingUpNext } from "@bui/loaders";
-import { Colors, Ripple, Wobble, WobbleNoText, Lava } from "@bui/buttons";
+import {
+  Colors,
+  Ripple,
+  Wobble,
+  WobbleNoText,
+  Lava,
+  Glow,
+  Magic,
+  MD3,
+} from "@bui/buttons";
 
 export default function Web() {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div className="flex flex-col gap-8 items-center justify-center h-screen bg-[#111]">
-      <Colors onClick={() => setIsLoading(!isLoading)}>Submit</Colors>
+    <div className="flex flex-col gap-24 items-center justify-center min-h-screen bg-[#002]">
+      <Colors onClick={() => setIsLoading(!isLoading)}>Colors</Colors>
       <Lava onClick={() => setIsLoading(!isLoading)}>Submit</Lava>
       <WobbleNoText onClick={() => setIsLoading(!isLoading)}>
-        Submit
+        Wobble No Text
       </WobbleNoText>
-      <Wobble onClick={() => setIsLoading(!isLoading)}>Submit</Wobble>
-      <Ripple onClick={() => setIsLoading(!isLoading)}>Submit</Ripple>
+      <Wobble onClick={() => setIsLoading(!isLoading)}>Wobble</Wobble>
+      <Ripple onClick={() => setIsLoading(!isLoading)}>Ripple</Ripple>
+      <Glow onClick={() => setIsLoading(!isLoading)}>Glow</Glow>
+      <Magic onClick={() => setIsLoading(!isLoading)}>Magic</Magic>
+      <MD3 onClick={() => setIsLoading(!isLoading)}>MD3</MD3>
+
       {/* <LoaderWrapper isLoading={isLoading} loader={<ComingUpNext />} /> */}
     </div>
   );
