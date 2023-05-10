a5658a0183084788ace47de640bfabb43f5f6fff
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-10 18:18:47 +0100

    add colors story

---
+++ apps/workshop/src/stories/Colors.stories.ts
@@ -0,0 +1,19 @@
+import type { StoryObj } from '@storybook/react';
+
+import { Colors } from '@bui/buttons';
+
+const meta = {
+  title: 'Buttons/Colors',
+  component: Colors,
+  tags: ['autodocs'],
+  argTypes: {
+    text: { control: 'text' },
+    color: { control: 'color' },
+    numberOfColors: { control: 'number', defaultValue: 5 },
+    duration: { control: 'number', defaultValue: 1 },
+  },
+}
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {};
