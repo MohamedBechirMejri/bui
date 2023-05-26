169b7db3bc9bbed174325e9d155dc46fc66c0207
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-26 05:44:16 +0100

    remove old styling

--- packages/forms/src/Inputs/InputZen.tsx
+++ packages/forms/src/Inputs/InputZen.tsx
@@ -4,11 +4,11 @@ import { useState } from "react";
 export const InputZen = ({
   value = "",
   setValue,
-  type = "text",
+  type = "email",
   placeholder = "Enter Email",
   width = "auto",
   height = "4rem",
-  color = "#e11d48",
+  color = "#000000",
   ...props
 }: {
   value?: string;
@@ -23,9 +23,14 @@ export const InputZen = ({
 
   return (
     <div
-      style={{ width, height, position: "relative" }}
       onClick={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
+      style={{
+        width,
+        height,
+        position: "relative",
+        border: isFocused ? `3px solid ${color}` : `3px solid #888`,
+      }}
     >
       <style>
         {`  /* clear the 'X' from Chrome */
@@ -36,87 +41,6 @@ export const InputZen = ({
             { display: none }
             `}
       </style>
-
-      <motion.div
-        style={{
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          color: isFocused ? color : "#888",
-          borderRadius: "1.25rem",
-          backgroundColor: isFocused ? color + 11 : `#ffffff`,
-        }}
-        animate={{
-          border: isFocused ? `2px solid ${color}` : `2px solid #888`,
-          boxShadow: isFocused ? `0 0 0 1px ${color}` : `0 0 0 0px ${color}00`,
-        }}
-      >
-        <AnimatePresence>
-          {/* placeholder */}
-          {!isFocused && value === "" && (
-            <motion.span
-              style={{
-                fontFamily: "inherit",
-                fontSize: "inherit",
-                fontWeight: "inherit",
-                color: "#888",
-                position: "absolute",
-                top: "50%",
-                left: "1rem",
-                userSelect: "none",
-              }}
-              initial={{ opacity: 0, y: "-100%" }}
-              animate={{ opacity: 1, y: "-50%" }}
-              exit={{ opacity: 0, y: "-100%" }}
-            >
-              {placeholder}
-            </motion.span>
-          )}
-        </AnimatePresence>
-        {/* label */}
-        <motion.span
-          style={{
-            fontSize: "0.75em",
-            color: "inherit",
-            position: "absolute",
-            top: "50%",
-            left: "1rem",
-            userSelect: "none",
-          }}
-          initial={{ opacity: 0, y: "-50%" }}
-          animate={{
-            opacity: isFocused ? 1 : [1, 0, 0, 0, 0, 0, 0, 0],
-            y: isFocused ? "-180%" : "-50%",
-          }}
-        >
-          {placeholder}
-        </motion.span>
-
-        {/* input */}
-        <motion.input
-          style={{
-            boxSizing: "border-box",
-            width: "100%",
-            height: "100%",
-            border: "none",
-            outline: "none",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            padding: "1rem",
-            paddingBlock: "0.25rem",
-            fontFamily: "inherit",
-            fontSize: "inherit",
-            fontWeight: "inherit",
-            color: "inherit",
-            backgroundColor: "transparent",
-          }}
-          type={type}
-          value={value}
-          onChange={e => setValue(e.target.value)}
-          {...props}
-        />
-      </motion.div>
     </div>
   );
 };
